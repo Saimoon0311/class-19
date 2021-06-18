4fa170d2fb2bd4500508dec3fbb10b69f83989e6
unknown <you@example.com>
2021-06-18 19:01:52 +0500

    class 19

---
+++ app.js
@@ -0,0 +1,41 @@
+    // var list = document.getElementById("list")
+// function addtodo(){
+//     var add_item = document.getElementById("todo-item")
+//     var li = document.createElement("li")
+//     var liText = document.createTextNode(add_item.value)
+//     li.appendChild(liText)
+//     list.appendChild(li)
+//     add_item.value=""
+
+
+//     var delBtn = document.createElement("button")
+//     var delText = document.createTextNode("Delete")
+//     delBtn.setAttribute("class","btn")
+//     delBtn.setAttribute("onclick","delitem(this)")
+//     delBtn.appendChild(delText)
+//     li.appendChild(delBtn)
+    
+
+//     var editBtn = document.createElement("button")
+//     var editText = document.createTextNode("Edit")
+//     editBtn.appendChild(editText)
+//     li.appendChild(editBtn)
+//     editBtn.setAttribute("onclick","editItem(this)")
+// }
+// function delitem(e){
+//     e.parentNode.remove()
+// }
+// function deleAll(){
+//     list.innerHTML= ""
+// }
+// function editItem(e){
+//     var editvalue =prompt("Enter Edit value",e.parentNode.firstChild.nodeValue)
+//    var val = e.parentNode.firstChild.nodeValue
+//    e.parentNode.firstChild.nodeValue = editvalue
+// }
+
+
+
+
+
+
